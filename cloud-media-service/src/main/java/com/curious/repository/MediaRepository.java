975700f252a71b1a9cceb14c2ab6332de53db97b
DonnieXu <604865895@qq.com>
2016-12-02 12:30:14 +0800

    add eureka

--- cloud-media-service/src/main/java/com/curious/repository/MediaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.curious.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curious.entity.Media;
-
-public interface MediaRepository extends JpaRepository<Media, Long> {
-
-}
